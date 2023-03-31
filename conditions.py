6bc07869242723ce8eba2c3d3d76662e0dffa59a
audieton <127110031+audieton@users.noreply.github.com>
2023-03-31 10:22:01 +0300

    Python Training

---
+++ conditions.py
@@ -0,0 +1,20 @@
+is_hot =False
+is_cold =True
+
+if is_hot:
+    print("it`s a hot day ")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It`s a cold day")
+    print("Wear warm clothes")
+        
+else:
+    print("It`s a lovely day enjoy your day")    
+    
+    # Task To Do 
+    #price of a house is $1M
+    #If buyer has good credit,
+    #they need to put down 10%
+    #Otherwise
+    #they need to put down 20%
+    #print the down payment
