b3c71c6f5a9dc37921c7b142d9df4b9bbfca0077
audieton <127110031+audieton@users.noreply.github.com>
2023-04-04 14:40:38 +0300

    Python Training

---
+++ summary.py
@@ -0,0 +1,10 @@
+def cubic (S):
+      S.A = 2(l+w)*(w+h)*(h+l)
+      return S.A
+l=float(input("Enter the lenght"))  
+w=float(input("Enter the width"))
+h=float(input("Enter the height")) 
+
+cubic()
+
+  
