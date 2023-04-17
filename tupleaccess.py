fb8bca6073c6cd129eb07979c62253549d60d30a
audieton <127110031+audieton@users.noreply.github.com>
2023-04-18 00:09:07 +0300

    Python Training

---
+++ tupleaccess.py
@@ -0,0 +1,19 @@
+#  change tuple value a tuple cannot be changed or removed in other words immutable
+# But they is a workaround a tuple can be changed into a list and 
+# the list can be changed back into a tuple
+
+x = ("apples","banana","cherry")
+y = list(x)
+y[1] ="kiwi"
+x = tuple(y)
+
+print(x)
+
+# Add Item
+
+thistuple= ("apple","banana","cherry")
+y =list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+
