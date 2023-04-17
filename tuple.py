dda11cad2f00e5c91ae561faecc6571081fec00d
audieton <127110031+audieton@users.noreply.github.com>
2023-04-17 23:41:37 +0300

    Python Training

---
+++ tuple.py
@@ -0,0 +1,34 @@
+# This is a tuple 
+mytuple= ("apple","pineapples","mangoes","apple","pineapple")
+print(mytuple)
+
+# Tuple of type() 
+thistuple =("apple",)
+print(type(thistuple))
+
+# Tuple can be a string, integer, boolean. 
+tuple1= ("apple","cherry","bananas")
+tuple2= (1,2.3,4.5)
+tuple3= (True,False,False)
+
+# Tuple can take an data type
+tuple1 = ("abc",34,True,40,"male")
+
+# Tuple contructor()
+thistuple= tuple(("apple","cherry","bananas"))
+print(thistuple)
+
+# Tuple Item indexing 
+thistuple=("apple","banana","cherry")
+print(thistuple[1])
+
+# Tuple Negative indexing
+thistuple=("apple","banana","cherry")
+print(thistuple[-1])
+
+# Range of Indexes
+thistuple=("apple","banana","cherry","orange","kiwi","melon","mango")
+print(thistuple[2:5])
+
+
+
